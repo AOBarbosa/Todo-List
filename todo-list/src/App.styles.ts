282af3d1c2105b55e5bcf4d133338643f48dcc6b
Andre Barbosa <andre.barbosa.076@ufrn.edu.br>
2022-11-14 17:08:13 -0300

    App working

--- todo-list/src/App.styles.ts
+++ todo-list/src/App.styles.ts
@@ -15,8 +15,8 @@ export const Area = styled.div`
 export const Header = styled.h1`
   margin: 0;
   padding: 0;
-  color: #FFF;
-   text-align: center;
-   border-bottom: 1px solid #444;
-   padding-bottom: 20px:
+  color: #fff;
+  text-align: center;
+  border-bottom: 1px solid #444;
+  padding-bottom: 20px;
 `;
